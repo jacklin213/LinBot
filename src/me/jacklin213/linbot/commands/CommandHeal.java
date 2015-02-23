00217226d3e863484f2a2409e565333f12512f4c
Jack Lin <jacklin213@gmail.com>
2015-02-23 14:14:42 +1300

    Add message to CmdHeal

--- src/me/jacklin213/linbot/commands/CommandHeal.java
+++ src/me/jacklin213/linbot/commands/CommandHeal.java
@@ -17,10 +17,13 @@ public class CommandHeal extends LinBotCommand {
 		if (args.length == 0) {
 			Player player = (Player) sender;
 			player.setHealth(20);
+			plugin.MSG.commandReply(sender, "&eYou have been &aHealed!");
 		} 
 		if (args.length == 1) {
 			Player tarPlayer = Bukkit.getPlayer(args[0]);
 			if (tarPlayer != null) tarPlayer.setHealth(20);
+			plugin.MSG.commandReply(sender, "&eYou have &aHealed &6 " + tarPlayer.getName());
+			plugin.MSG.commandReply(sender, "&eYou have been &aHealed &eby &6" + sender.getName());
 		}
 	}
 
